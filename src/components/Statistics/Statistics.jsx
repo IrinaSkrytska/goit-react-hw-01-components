1ff0320fbb87af8d0156c827fb3a171c925812d4
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2022-07-10 21:28:53 +0300

    fixes

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,16 +1,12 @@
 import propTypes from 'prop-types';
 import css from './Statistics.module.css';
-
-
-function setBg() {
-  return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
-}
+import { setBg } from 'components/Helpers/RandomColor';
 
 
 export const Statistics = ({title, stats}) => {
   return (<section>
-  <h2> </h2>
-        {title ? <h2 className={css.title}>{title}</h2> : ''}
+  {title && <h2 className={css.title}>{title}</h2>} 
+        
         
       <ul className={css.statistics__thumb}>
             {stats.map(el => (<li className={css.statistics__number} key={el.id} style={{backgroundColor: setBg()}} >
